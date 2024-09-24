b238fd1308a2f3de598b8c3c512d67cef19bb2f0
Zahed75 <zahedhasan.cs@gmail.com>
2024-09-24 16:28:38 +0600

    productFeature images done

--- provider/views.py
+++ provider/views.py
@@ -38,7 +38,7 @@ def upload_multiple_images(request):
 def get_all_images(request):
     try:
         images = ProductImage.objects.all()
-        serializer = ProductImageSerializer(images, many=True)
+        serializer = ProductImageSerializer(images, many=True,context={'request': request})
         return Response({
             'code': status.HTTP_200_OK,
             'message': 'Get All Images Fetched Successfully',
@@ -50,3 +50,9 @@ def get_all_images(request):
             'code': status.HTTP_400_BAD_REQUEST,
             'message': str(e)
         })
+
+
+
+
+
+
